2ed06f63d584727f974825edbc82dd55ea2a1f07
fmughalishere <fizamuneer0101@gmail.com>
2026-05-16 17:34:13 +0500

    Updated

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://easyjobspk.onrender.com/api', 
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://easyjobspk.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
